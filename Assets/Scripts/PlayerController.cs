7970bc7248513b30540a3b82bd2094a558eb4721
chevre hurlante lol <paul.breuza@gmail.com>
2021-03-19 15:27:27 +0100

    Original commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+   private float speedRotation = 10f;
+   private float speed = 5f;
+   private Vector2 _movementInput;
+   private Vector2 _rotationInput;
+   
+   private void Update()
+   {
+      transform.Translate(new Vector3(_movementInput.x, 0, _movementInput.y) * speed * Time.deltaTime, Space.World);
+      transform.Rotate(0, _rotationInput.x, 0);
+
+   }
+
+   /*private void FixedUpdate()
+   {
+      transform.Rotate(0, _rotationInput.x, 0);
+   }*/
+
+   public void OnMove(InputAction.CallbackContext ctx) => _movementInput = ctx.ReadValue<Vector2>();
+
+   public void OnRotate(InputAction.CallbackContext ctx) => _rotationInput = ctx.ReadValue<Vector2>();
+}
---
+++ Assets/Scripts/gun.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class gun : MonoBehaviour
+{
+    public float range = 100f;
+    public float damage = 10f;
+
+    private bool shooted = false;
+
+
+    void Update()
+    {
+     /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }*/
+
+        if (shooted)
+        {
+            Shoot();
+            Debug.Log("Crepe");
+        }
+    }
+
+    public void OnShoot(InputAction.CallbackContext ctx)
+    {
+        shooted = ctx.ReadValue<bool>();
+        shooted = ctx.action.enabled;
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}

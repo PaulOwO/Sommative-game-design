4f8d1b52002150a3561113f017361b121653e3c2
Victor gonnet <vicy@live.Fr>
2021-03-23 22:19:31 +0100

    New scene, arena + test

---
+++ Assets/Scripts/ArenaManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class ArenaManager : MonoBehaviour
+{
+    //private float speed = 50f;
+    private void Update()
+    {
+        StartCoroutine(ExecuteAfterTime(1));
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        /*yield return new WaitForSeconds(10f);
+
+        if (GameObject.FindWithTag("Obstacles"))
+        {
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }*/
+        
+        yield return new WaitForSeconds(60f);
+ 
+        Destroy(GameObject.FindWithTag("Obstacles"));
+    }
+
+   
+}

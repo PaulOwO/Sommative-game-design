3f250d3440b706b433097567dfd2ebda953f4ff2
chevre hurlante lol <paul.breuza@gmail.com>
2021-03-27 15:19:22 +0100

    Main Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using InControl;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool GameIsPaused = false; 
+
+    [SerializeField] GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.Escape)) || (InputManager.ActiveDevice.Action3.WasPressed))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    private void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+        Debug.Log("Loading Menu");
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting game");
+    }
+}

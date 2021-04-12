c5822c8f37c8db8062e49f679a4345df38aaaf0e
chevre hurlante lol <paul.breuza@gmail.com>
2021-04-12 14:54:24 +0200

    Push on impact

--- Assets/Scripts/gun.cs
+++ Assets/Scripts/gun.cs
@@ -9,6 +9,7 @@ public class gun : MonoBehaviour
 {
     public float range = 100f;
     public float damage = 10f;
+    public float impactForce = 30f;
     public LineRenderer lineRenderer;
     public bool shootAvailable = true;
     [SerializeField] private PlayerController playercontroller;
@@ -39,7 +40,12 @@ public class gun : MonoBehaviour
             Target target = hit.transform.GetComponent<Target>();
             if (target != null)
             {
-                target.TakeDamage(damage);
+               // target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
             }
 
             lineRenderer.SetPosition(0, transform.position);

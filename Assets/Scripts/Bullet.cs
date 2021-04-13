7e2c92eab3dbd05901324720bc5f09017699b511
chevre hurlante lol <paul.breuza@gmail.com>
2021-04-13 20:42:47 +0200

    Shoot bullet almost done

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    
+  
+    private const float bulletSpeed = 0.1f;
+    [SerializeField] private GameObject firePoint;
+
+
+
+    void Update()
+    {
+        //transform.position += transform.forward * bulletSpeed * Time.deltaTime;
+        transform.position += firePoint.transform.forward * bulletSpeed;
+    }
+}
